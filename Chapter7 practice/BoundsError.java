93d4482dfbc9852e97da6c4f7c6bdc8bc06a1ccd
AllenLin001 <allenlin41@yahoo.com>
2014-11-18 12:21:28 -0600

    in class practice

---
+++ Chapter7 practice/BoundsError.java
@@ -0,0 +1,14 @@
+public class BoundsError
+{ 
+    public static void main (String [] args)
+    {
+        int [] scores = new int [10];
+        
+        for (int i = 1; i<= 10; i++)
+        {
+            scores[i]=i+1;
+        }
+    }
+}
+
+
